4661f3dd7da585ef7a1d7117a9170bf470696827
sft96 <kulichkov.sft@gmail.com>
2025-09-30 23:41:02 +0300

    change of functions

---
+++ data_processing/functions.py
@@ -0,0 +1,104 @@
+'''
+В данном модуле реализованы несколько пользовательских
+классов и функций для быстрой аналитики больших данных.
+'''
+import subprocess
+from configuration import spark
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+import pandas as pd
+
+
+class Data:
+
+    '''
+    Классы, расширяющие базовые возможности
+    фреймворка pyspark для быстрой аналитики.
+    '''
+
+    _first: int = 0
+    _last: int = -1
+    for_console: int = 999
+
+    @staticmethod
+    def writeExcel():
+        '''Удобная запись данных в excel-файл.'''
+
+        pass
+
+    @staticmethod
+    def viewEntries():
+        '''Дополненный метод show() для просмотра.'''
+
+        pass
+
+
+class Subcriptions(Data):
+
+    '''
+    Класс для просмотра основной информации
+    по конкретным источникам данных.
+    '''
+
+    @staticmethod
+    def showDatabases():
+        '''Показать интересующие источники данных.'''
+
+        pass
+
+    @staticmethod
+    def showTables():
+        '''Показать таблицы источника данных.'''
+
+        pass
+
+    @staticmethod
+    def showAttributes():
+        '''Показать атрибуты таблицы с описанием.'''
+
+        pass
+
+    @staticmethod
+    def showPartitions():
+        '''Показать партиции/разделы таблицы.'''
+
+        pass
+
+
+class Parquet(Data):
+
+    '''Класс работы с parquet-файлами.'''
+
+    @staticmethod
+    def getPaths():
+        '''Получить все пути к партициям/разделам таблицы.'''
+
+        pass
+
+    @staticmethod
+    def getOne():
+        '''
+        Получить один parquet-файл.
+
+        Для корректной работы забираем единственный/последний
+        файл, так как первый может быть служебным маркером "SUCCESS",
+        а не набором данных.
+        '''
+
+        pass
+
+    @staticmethod
+    def getFrame():
+        '''
+        Получить фрейм данных из одного parquet-файла.
+
+        Ускорение обращения к набору данных за счёт чтения одного файла,
+        вместо всей таблицы:
+        -> spark.table('database.table')
+        -> spark.read.parquet('file_name')
+
+        Полезно, если нужно быстро проанализировать данные
+        или собрать прототип витрины.
+        '''
+
+        pass
